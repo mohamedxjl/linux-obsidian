e5876c2470d378550805cc3506bf5f562fbb2bd5
xjlsigmaboy <85705200+xjlsigmaboy@users.noreply.github.com>
2024-10-18 19:51:56 +0300

    Update and rename file to clonesource.sh

---
+++ src/clonesource.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+version=6
+patchlevel=11
+sublevel=4
+name='obsidian'
+source=https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$version.$patchlevel.$sublevel.tar.xz
+
+wget $source
+
+tar xvf linux-$version.$patchlevel.$sublevel.tar.xz
