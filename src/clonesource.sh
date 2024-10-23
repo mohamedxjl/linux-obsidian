3b42d9b1a72bb35e2c89c06af741e4dd2533ca0d
xjlsigmaboy <85705200+xjlsigmaboy@users.noreply.github.com>
2024-10-23 11:03:33 +0300

    Update source.sh

--- src/clonesource.sh
+++ src/clonesource.sh
@@ -7,5 +7,6 @@ subname='obsidian'
 source=https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$version.$patchlevel.$sublevel.tar.xz
 
 wget $source
-
+mkdir linux-obsidian-$version.$patchlevel.$sublevel
+cd linux-obsidian-$version.$patchlevel.$sublevel
 tar xvf linux-$version.$patchlevel.$sublevel.tar.xz
